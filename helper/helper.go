24ddde1f7da62ea944c20136b3dab35516c9675c
fadhrila_fasya <fadhrilafasya@gmail.com>
2025-10-06 14:31:01 +0700

    ACCESS MODIFIER

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,12 @@
 package helper
 
+var version = "1.0.0"
+var Application = "Belajar Golang Dasar"
+
+func sayGoodbye(name string) string {
+	return "Goodbye " + name
+} // private (tidak bisa diakses diluar package helper)
+
 func SayHello(name string) string {
 	return "Hello " + name
-}
+} // public (bisa diakses diluar package helper)
--- import.go
+++ import.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	result := helper.SayHello("John")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.sayGoodbye("John")) // error, karena function sayGoodbye diawali dengan huruf kecil (private)
 }
