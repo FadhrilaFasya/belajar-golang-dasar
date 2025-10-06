c9286758c4967c91420bcc7decae79da686669c8
fadhrila_fasya <fadhrilafasya@gmail.com>
2025-10-06 14:26:22 +0700

    PACKAGE & IMPORT

---
+++ import.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"belajar-golang-dasar/helper"
+	"fmt"
+) // import package (menggunakan package helper
+
+func main() {
+	result := helper.SayHello("John")
+	fmt.Println(result)
+}
